8828781bcfe8394e19b401493c9729ed7310fcbf
Caleb Hessing <cckkhh7@gmail.com>
2026-03-06 15:32:32 -0700

    finding headers looks good

--- cs324/toc-generator.py
+++ cs324/toc-generator.py
@@ -5,14 +5,37 @@ Table-of-contents generator for man-notes.md
 import re
 
 MAN_NOTES_FILEPATH = './man-notes.md'
+MAN_NOTES_FIRST_HEADER = "# man notes"
+IS_FIRST_HEADER = lambda s: s.strip() == MAN_NOTES_FIRST_HEADER
 
-def regex_search_file(filepath: str, pattern: str) -> list:
-    txt = ''
-    with open(filepath) as f:
-        txt = f.read()
-    return re.findall(pattern, txt)
+def identify_headers(file) -> list:
+    headers = []
+
+    in_code_block = False
+    for poopy_line in file:
+        line = poopy_line.strip()
+        
+        if re.match("```[a-zA-Z0-9]*", line):
+            in_code_block = not in_code_block
+        if in_code_block:
+            continue
+
+        if re.match("#+[\\x20-\\xFF]+", line):
+            headers.append(line)
+
+    return headers
+
+def main() -> None:
+    headers = []
+
+    with open(MAN_NOTES_FILEPATH) as f:
+        # move to `# man notes`
+        while (not IS_FIRST_HEADER(next(f))):
+            pass
+        headers = identify_headers(f)
+    
+    print(headers)
+
 
 if __name__ == "__main__":
-    x = regex_search_file(MAN_NOTES_FILEPATH, "^##*[^\n]*") # gets close but matches w/ #include lines in code
-                                                            # uhhh wait actually that's awkward it works on regex101.com but not here w/ in Python
-    print(x)
+    main()
